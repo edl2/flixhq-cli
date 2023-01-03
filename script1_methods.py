4832d3c122b75c71682d76379b0e2aaf2aeef360
edl2 <95947898+edl2@users.noreply.github.com>
2023-01-03 18:12:46 -0500

    Update script1_methods.py

--- script1_methods.py
+++ script1_methods.py
@@ -14,3 +14,16 @@ def get_selected_stream(data):
         print(f"{i+1}: {quality}")
     selected_index = int(input("Please select a stream: ")) - 1
     return selected_index
+
+def get_selected_episode(data):
+    seasons = data["episodes"]
+    last_season = max(episode['season'] for episode in seasons)
+    selected_season = int(input(f"Choose season 1-{last_season}: "))
+    selected_episodes = [episode for episode in seasons if episode['season'] == selected_season]
+    for episode in selected_episodes:
+        print(episode['title'])
+    selected_episode_number = int(input("Choose episode: "))
+    selected_episode_id = {episode['number']: episode['id'] for episode in selected_episodes}
+    for selected_episode_number in selected_episode_id:
+        episode_id = selected_episode_id[selected_episode_number]
+    return episode_id
