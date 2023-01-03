669085c06de04be7d2806468778643671da045db
edl2 <95947898+edl2@users.noreply.github.com>
2023-01-03 18:13:09 -0500

    Update script1_methods.py

--- script1_methods.py
+++ script1_methods.py
@@ -1,28 +1,29 @@
+import subprocess
 def get_movie_show_input():
-    query = input("Enter movie/show: ")
+    query = subprocess.check_output(["rofi", "-dmenu", "-p", "Enter movie/show"]).decode("utf-8")
     return query
 
 def get_selected_media_index(title_with_id_type):
-    for i, title in enumerate(title_with_id_type):
-        print(f"{i+1}: {title}")
-    selected_index = int(input("Please select your media: ")) - 1
+    options = "\n".join([title for title in title_with_id_type]).encode()
+    output = subprocess.run(["rofi", "-dmenu", "-p", "Please select your media"], input=options, capture_output=True).stdout.strip().decode()
+    selected_index = title_with_id_type.index(output)
     return selected_index
 
 def get_selected_stream(data):
-    for i, source in enumerate(data["sources"]):
-        quality = source["quality"]
-        print(f"{i+1}: {quality}")
-    selected_index = int(input("Please select a stream: ")) - 1
+    options = "\n".join([source["quality"] for source in data["sources"]]).encode()
+    selected_quality = subprocess.run(["rofi", "-dmenu", "-p", "Please select a stream"], input=options, capture_output=True).stdout.strip().decode()
+    selected_index = next(i for i, source in enumerate(data["sources"]) if source["quality"] == selected_quality)
     return selected_index
 
 def get_selected_episode(data):
     seasons = data["episodes"]
     last_season = max(episode['season'] for episode in seasons)
-    selected_season = int(input(f"Choose season 1-{last_season}: "))
+    selected_season = int(subprocess.run(["rofi", "-dmenu", "-p", f"Choose season 1-{last_season}"], capture_output=True).stdout.strip().decode())
     selected_episodes = [episode for episode in seasons if episode['season'] == selected_season]
-    for episode in selected_episodes:
-        print(episode['title'])
-    selected_episode_number = int(input("Choose episode: "))
+    options = "\n".join([episode["title"] for episode in selected_episodes]).encode()
+    selected_title = subprocess.run(["rofi", "-dmenu", "-p", "Please select an episode"], input=options, capture_output=True).stdout.strip().decode()
+    selected_index = next(i for i, episode in enumerate(selected_episodes) if episode["title"] == selected_title)
+    selected_episode_number = selected_episodes[selected_index]['number']
     selected_episode_id = {episode['number']: episode['id'] for episode in selected_episodes}
     for selected_episode_number in selected_episode_id:
         episode_id = selected_episode_id[selected_episode_number]
